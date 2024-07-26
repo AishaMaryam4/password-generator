2cff2fc829674b24d8f19b6097688db4ff5bb70b
AishaMaryam <142927390+AishaMaryam4@users.noreply.github.com>
2024-07-26 20:22:51 +0530

    Update Password.java

--- Password Generator/src/Password.java
+++ Password Generator/src/Password.java
@@ -11,21 +11,21 @@ public class Password {
     public int CharType(char C) {
         int val;
 
-        // Char is Uppercase Letter
+        
         if ((int) C >= 65 && (int) C <= 90)
             val = 1;
 
-        // Char is Lowercase Letter
+        
         else if ((int) C >= 97 && (int) C <= 122) {
             val = 2;
         }
 
-        // Char is Digit
+        
         else if ((int) C >= 60 && (int) C <= 71) {
             val = 3;
         }
 
-        // Char is Symbol
+        
         else {
             val = 4;
         }
